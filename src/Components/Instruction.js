ce3341ebdf570397a3e2746d01a1c27f3a23e0fe
Marta Budnik <mbudnikm97@gmail.com>
2019-04-23 22:47:45 +0200

    change style and add list to instruction

--- src/Components/Instruction.js
+++ src/Components/Instruction.js
@@ -4,6 +4,10 @@ const Instruction = () => {
     return (
         <div>
             <h1 style={{color: '#124191'}}>Instrukcja</h1>
+            <ul>
+                <li>Upewnij się, że kamera jest włączona</li>
+                <li>Twoja twarz powinna być odsłonięta i widoczna przez kamerę</li>
+            </ul>
         </div>
     )
 }
