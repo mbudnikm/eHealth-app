b5b36be44e6ee78bc4610074daf2984ef16335d8
Marta Budnik <mbudnikm97@gmail.com>
2019-04-28 21:13:42 +0200

    add axios functions

--- src/Shared/services.js
+++ src/Shared/services.js
@@ -1,2 +1,25 @@
+import axios from "axios";
+
 const baseURL = "http://localhost:8090//api/v1/users";
 
+export const registerUser = (payload) => {
+    axios({
+        method: 'post',
+        url: baseURL,
+        data: payload
+    }).then(response => console.log(response));
+}
+
+export const getUserDetails = (id) => {
+    axios({
+        method: 'get',
+        url: `baseURL/${id}`,
+    }).then(response => console.log(response));
+}
+
+export const deleteUser = (id) => {
+    axios({
+        method: 'delete',
+        url: `baseURL/${id}`,
+    }).then(response => console.log(response));
+}
