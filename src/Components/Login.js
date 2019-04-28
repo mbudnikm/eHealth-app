405e22d7fe6a9f5738579494d77bf618f0342b60
Marta Budnik <mbudnikm97@gmail.com>
2019-04-28 20:10:51 +0200

    centered login form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-  } from "react-router-dom";
 import Register from "./Register"
   
 
@@ -19,26 +14,28 @@ class Login extends Component {
 
     render() {
         return (
-            <Router>
-                <div>
-                    <Link to="/register" className="float-right p-4">Nie masz jeszcze konta? Zarejestruj się</Link>
-                    <div className="d-flex flex-column col-4">
-                        <h2 className="p-4">Logowanie</h2>
-                        <form>
-                        <div className="form-group mb-4">
-                            <label for="userLoginInput" className="float-left">Nazwa użytkownika</label>
-                            <input type="text" className="form-control form-control-lg" id="userLoginInput" placeholder="Wpisz nazwę użytkownika..." />
+                <div className="container">
+                    <div className="d-flex justify-content-end p-4">
+                        <a href="/register">Nie masz jeszcze konta? Zarejestruj się</a>
+                    </div>
+                    <div className="container h-100">
+                        <div className="col-4 mx-auto">
+                            <h2 className="p-4">Logowanie</h2>
+                            <form>
+                            <div className="form-group mb-4">
+                                <label for="userLoginInput" className="float-left">Nazwa użytkownika</label>
+                                <input type="text" className="form-control form-control-lg" id="userLoginInput" placeholder="Wpisz nazwę użytkownika..." />
+                            </div>
+                            <div className="form-group mb-4">
+                                <label for="userPassowordInput" className="float-left">Hasło</label>
+                                <input type="password" className="form-control form-control-lg" id="userPasswordInput" placeholder="Wpisz hasło użytkownika..." />
+                            </div>
+                            <button type="submit" className="btn btn-primary btn-lg" style={{ width: '100%' }}>Zaloguj się</button>
+                            </form>
                         </div>
-                        <div className="form-group mb-4">
-                            <label for="userPassowordInput" className="float-left">Hasło</label>
-                            <input type="password" className="form-control form-control-lg" id="userPasswordInput" placeholder="Wpisz hasło użytkownika..." />
-                        </div>
-                        <button type="submit" className="btn btn-primary btn-lg" style={{ width: '100%' }}>Zaloguj się</button>
-                        </form>
                     </div>
                 </div>
-                <Route path="/register" component={Register} />
-            </Router>
+                
         );
     }
 }
