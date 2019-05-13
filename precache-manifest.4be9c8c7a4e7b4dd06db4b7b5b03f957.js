78b0fe3d6f87f32492b91c451478049726229782
Marta Budnik <marta.budnik2@atos.net>
2019-05-13 07:44:51 +0200

    Updates

---
+++ precache-manifest.4be9c8c7a4e7b4dd06db4b7b5b03f957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b284953ac682a4789e5a0b3f115e64b",
+    "url": "/eHealth-app/static/media/whirlpool.9b284953.png"
+  },
+  {
+    "revision": "b4ec773f9abc321425a5",
+    "url": "/eHealth-app/static/js/runtime~main.f7876d80.js"
+  },
+  {
+    "revision": "6af0efe321e8c9a2b9a6",
+    "url": "/eHealth-app/static/js/main.2f93daa4.chunk.js"
+  },
+  {
+    "revision": "de65a95083b68881f285",
+    "url": "/eHealth-app/static/js/2.e537d8bc.chunk.js"
+  },
+  {
+    "revision": "6af0efe321e8c9a2b9a6",
+    "url": "/eHealth-app/static/css/main.4630bf0f.chunk.css"
+  },
+  {
+    "revision": "48b2579341adf5d6d0ff521bf897643b",
+    "url": "/eHealth-app/index.html"
+  }
+];
