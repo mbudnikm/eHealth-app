9511c94e96a6464ff706f314fd0d080484cf9f93
Marta Budnik <mbudnikm97@gmail.com>
2019-06-02 21:39:21 +0200

    pulse results per week

--- src/Components/PulseChart.js
+++ src/Components/PulseChart.js
@@ -11,14 +11,24 @@ class PulseChart extends Component {
     super(props)
     this.state = {
       pulseMeasure: [],
+      currentWeek: parseInt(moment().format('W')),
+      week: parseInt(moment().format('W')),
+      measurements: [],
+      groupedByWeek: {},
+      weeksArray: [],
+      groupedArray: []
     }
   }
 
-  async componentDidMount() {
+  async componentWillMount() {
     const pulseMeasure = await handleResponse(async () => await getPulse(this.props.userInfo))
-    pulseMeasure.length && this.setState({ pulseMeasure: pulseMeasure })
-    console.log(pulseMeasure)
-  };
+    this.setState({ 
+        pulseMeasure: pulseMeasure, 
+        groupedByWeek: this.datesGroupByComponent(pulseMeasure, 'W'),
+        weeksArray: Object.keys(this.datesGroupByComponent(pulseMeasure, 'W')),
+        groupedArray: Object.entries(this.datesGroupByComponent(pulseMeasure, 'W'))
+    }, this.setMeasurments)
+  }
 
   datesGroupByComponent = (dates, token) => {
     return dates.reduce(function(val, obj) {
@@ -28,12 +38,28 @@ class PulseChart extends Component {
     }, {});
   }
 
+  setMeasurments = () => {
+    if (this.state.weeksArray.includes(this.state.week.toString())){
+      const idx = this.state.weeksArray.indexOf(this.state.week.toString())
+      this.setState({ measurements: this.state.groupedArray[idx][1]})
+    } else {
+      this.setState({measurements: []})
+    }
+  }
+
+  previuosWeek = () => {
+    this.setState({week: this.state.week - 1}, this.setMeasurments)
+  }
+
+  nextWeek = () => {
+    this.setState({week: this.state.week + 1}, this.setMeasurments)
+  }
+
   render() {
-    console.log('W', this.datesGroupByComponent(this.state.pulseMeasure, 'w'))
-    const dataPoints = this.state.pulseMeasure.map(measurement => ({
+    const dataPoints = this.state.measurements.map(measurement => ({
       x: new Date(measurement.createdAt),
       y: measurement.pulse
-    })).filter(el => el.x.toString().includes('May'))
+    }))
 
 
     const options = {
@@ -57,10 +83,26 @@ class PulseChart extends Component {
       }]
     }
 
+    const weekStartDate = moment().day("Monday").week(this.state.week).startOf('isoWeek').format("DD.MM")
+    const weekEndDate = moment().day("Monday").week(this.state.week).endOf('isoWeek').format("DD.MM")
+
     return (
-      <div className="container mx-auto" style={{width: '700px'}}>
-      { this.state.pulseMeasure.length ? <CanvasJSChart options={options} /> : <h4>Brak pomiarów</h4>}
-      </div>
+      <>
+        <div className="container mt-2 mb-2 d-flex flex-row justify-content-around" style={{width: '700px'}}>
+          <button className="btn btn-outline-primary" onClick={this.previuosWeek}>
+            <i className="fa fa-angle-left" /> Poprzedni tydzień
+          </button>
+          <h2>{weekStartDate} - {weekEndDate}</h2>
+          { this.state.week < this.state.currentWeek && <button className="btn btn-outline-primary" onClick={this.nextWeek}>
+                Następny tydzień <i className="fa fa-angle-right" />
+          </button>}
+        </div>
+        <div className="container mx-auto" style={{width: '700px'}}>
+          { this.state.measurements.length ? 
+              <CanvasJSChart options={options} /> 
+          : <h4 className="p-5">Brak pomiarów</h4>}
+        </div>
+      </>
     );
   }
 }
